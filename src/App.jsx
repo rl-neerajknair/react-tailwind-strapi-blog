1a7540530394bf819c6472edc3e07f560ede60c0
RunelightRL <87036518+RunelightRL@users.noreply.github.com>
2024-07-13 20:47:24 +0530

    Data fetch completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { Homepage, BlogContentPage } from "./pages"
 import { Routes, Route } from "react-router-dom"
+import useFetch from './hooks/useFetch'
 
 export default function App() {
+  let { loading, data, error } = useFetch('http://localhost:1337/api/blogs?populate=*')
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+  console.log(data)
+
   return (
     <div>
       <Routes>
