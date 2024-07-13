c6754fdd3f64f6b6ac68e634c7db4bfffa6a0f43
RunelightRL <87036518+RunelightRL@users.noreply.github.com>
2024-07-13 20:11:53 +0530

    Basic Routes completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Homepage, BlogContentPage } from "./pages"
+import { Routes, Route } from "react-router-dom"
 
 export default function App() {
   return (
     <div>
-      <BlogContentPage />
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/blog/:id" element={<BlogContentPage />}></Route>
+      </Routes>
     </div>
   )
 }
